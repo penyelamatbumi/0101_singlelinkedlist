d2d98a24d5169b61748df39d42dad57316418a19
penyelamatbumi <inihanip7@gmail.com>
2026-04-23 12:23:49 +0700

    Menambahkan class LinkedList dan constructor

--- SinglelinkedList.cpp
+++ SinglelinkedList.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node 
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    
