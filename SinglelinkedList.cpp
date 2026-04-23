62dfecfa0fa649fc41306e8732d6433a4966e849
penyelamatbumi <inihanip7@gmail.com>
2026-04-23 12:26:26 +0700

    implementasi fungsi listEmpty dan traverse

--- SinglelinkedList.cpp
+++ SinglelinkedList.cpp
@@ -19,4 +19,60 @@ public:
         START = NULL;
     }
 
-    
+    void addNote()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim == START ->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current ->next;
+        }
+
+        
